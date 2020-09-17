cddee49e7521bc842dc5dfb4a04a2b92b6466024
z80sparrow <sparrow@oct.stars.ne.jp>
2020-09-17 22:15:56 +0900

    New!

---
+++ src/main/java/jp/prototype/api/Application.java
@@ -0,0 +1,13 @@
+package jp.prototype.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
